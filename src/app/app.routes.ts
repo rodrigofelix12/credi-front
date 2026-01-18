6c59b375db3e5474248a2cfd0e93c4743d34826c
Rodrigo Felix <rodrigo.felix321@gmail.com>
2026-01-17 23:47:24 -0300

    feat: adjusts in project to use components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'clientes',
+    loadComponent: () =>
+      import('./pages/clientes/clientes-list/clientes-list')
+        .then(c => c.ClientesList)
+  },
+  {
+    path: 'clientes/novo',
+    loadComponent: () =>
+      import('./pages/clientes/cliente-form/cliente-form')
+        .then(c => c.ClienteForm)
+  },
+  {
+    path: 'clientes/:id/editar',
+    loadComponent: () =>
+      import('./pages/clientes/cliente-form/cliente-form')
+        .then(c => c.ClienteForm)
+  },
+  {path: '', redirectTo: 'clientes', pathMatch: 'full'},
+  {path: '**', redirectTo: 'clientes'}
+];
---
+++ src/app/core/models/cliente.model.ts
@@ -0,0 +1,8 @@
+export interface Cliente {
+  id: number;
+  name: string;
+  cpf?: string;
+  rg?: string;
+  endereco?: string;
+  telefone?: string;
+}
